25f92115e3f0827f9757c9489dc302981e12c276
DAWGY69 <93137586+DAWGY69@users.noreply.github.com>
2023-11-17 13:33:53 +0100

    Update Object Renamer.py

--- Object Renamer.py
+++ Object Renamer.py
@@ -10,20 +10,24 @@ def rename_hierarchy(root, prefix, index=1, padding=2):
         index (int): The starting index for the suffix.
         padding (int): The number of digits for the suffix padding.
     """
+    # Get the list of all descendants of the root object, including the root itself
     children = cmds.listRelatives(root, allDescendents=True, fullPath=True)
     objects = [root] + children if children else [root]
 
+    # Iterate through each object in the hierarchy
     for obj in objects:
-        # Generate the new name
-        suffix = str(index).zfill(padding)
-        new_name = f"{prefix}_{suffix}"
+        # Generate the new name with the specified prefix and index
+        suffix = str(index).zfill(padding)  # Convert the index to a string with zero-padding
+        new_name = f"{prefix}_{suffix}"  # Combine the prefix and suffix to create the new name
 
-        # Rename the object
+        # Rename the object using Maya's rename command
         cmds.rename(obj, new_name)
 
-        # Increment the index
+        # Increment the index for the next object
         index += 1
 
 # Example usage
-root_object = cmds.ls(selection=True)[0]  # Get the currently selected root object
+# Get the currently selected root object
+root_object = cmds.ls(selection=True)[0]
+# Rename the hierarchy starting with the specified prefix, index, and padding
 rename_hierarchy(root_object, "MyObject", index=1, padding=3)
